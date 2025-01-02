999ca8eb4d6d50ca78f4a750c3d786aedb624ffa
Tirumalareddysai <tirumalareddysai2004@gmail.com>
2025-01-02 20:20:35 +0530

    url fix

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    credentials: true
+}));
 app.use(express.json());
 
 // Global error handler
